3b36f388d170c6e21fdab5be24f5af4e79a132a9
s-matyukevich <s.matyukevich@gmail.com>
2018-06-22 12:36:10 -0600

    broker added

--- ci/tasks/deploy-mysql.sh
+++ ci/tasks/deploy-mysql.sh
@@ -2,4 +2,13 @@
 
 set -o pipefail
 
-bosh -n -d mysql deploy concourse-mysql-deployment/ci/manifests/mysql.yml
+bosh -n -d mysql deploy concourse-mysql-deployment/ci/manifests/mysql.yml \
+    -v cf_api_url_dc1=$CF_API_URL_DC1 \
+    -v cf_admin_password_dc1=$CF_ADMIN_PASSWORD_DC1 \
+    -v cf_skip_ssl_validation_dc1=$CF_SKIP_SSL_VALIDATION_DC1 \
+    -v cf_api_url_dc2=$CF_API_URL_DC2 \
+    -v cf_admin_password_dc2=$CF_ADMIN_PASSWORD_DC2 \
+    -v cf_skip_ssl_validation_dc2=$CF_SKIP_SSL_VALIDATION_DC2 \
+    -v broker_ip_dc1=$BROKER_IP_DC1 \
+    -v broker_ip_dc2=$BROKER_IP_DC2 
+
