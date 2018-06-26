cd00104303aee1d57f3ef66088d28997e36e3dd1
s-matyukevich <s.matyukevich@gmail.com>
2018-06-26 16:34:10 -0600

    test

--- ci/tasks/deploy-mysql.sh
+++ ci/tasks/deploy-mysql.sh
@@ -2,6 +2,12 @@
 
 set -o pipefail
 
+mysql_ips_dc1_raw=${MYSQL_IPS_DC1/[/}}
+mysql_ips_dc1_raw=${mysql_ips_dc1_raw/]/}}
+mysql_ips_dc2_raw=${MYSQL_IPS_DC2/[/}}
+mysql_ips_dc2_raw=${mysql_ips_dc2_raw/]/}}
+combined_ips="[$mysql_ips_dc1_raw,$mysql_ips_dc2_raw]"
+combined_instances=$(echo "$combined_ips" | yaml2json | jq "map_values({address: .})")
 bosh -n -d mysql deploy concourse-mysql-deployment/ci/manifests/mysql.yml \
     -v cf_api_url_dc1=$CF_API_URL_DC1 \
     -v cf_admin_password_dc1=$CF_ADMIN_PASSWORD_DC1 \
@@ -9,6 +15,11 @@ bosh -n -d mysql deploy concourse-mysql-deployment/ci/manifests/mysql.yml \
     -v cf_api_url_dc2=$CF_API_URL_DC2 \
     -v cf_admin_password_dc2=$CF_ADMIN_PASSWORD_DC2 \
     -v cf_skip_ssl_validation_dc2=$CF_SKIP_SSL_VALIDATION_DC2 \
+    -v mysql_ips_dc1=$MYSQL_IPS_DC1 \
+    -v mysql_ips_dc2=$MYSQL_IPS_DC2 \
+    -v combined_mysql_instances="$combined_instances" \
+    -v proxy_ip_dc1=$PROXY_IP_DC1 \
+    -v proxy_ip_dc2=$PROXY_IP_DC2 \
     -v broker_ip_dc1=$BROKER_IP_DC1 \
     -v broker_ip_dc2=$BROKER_IP_DC2 
 
