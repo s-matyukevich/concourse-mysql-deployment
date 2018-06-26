b2e1db3587652b7edcdf514b2ad59bac33d90205
s-matyukevich <s.matyukevich@gmail.com>
2018-06-26 17:47:13 -0600

    check tag before building release

--- ci/tasks/build-and-upload-releases.sh
+++ ci/tasks/build-and-upload-releases.sh
@@ -32,5 +32,7 @@ cd ..
 
 
 #bosh -d mysql -n upload-release vmware-cf-mysql/cf-mysql-release.tgz
-bosh -d mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
+if ! bosh --json stemcells | jq -e --arg STEMCELL_VERSION $STEMCELL_VERSION '.Tables[0].Rows[] | select(.name=="bosh-vsphere-esxi-ubuntu-trusty-go_agent" and (.version | contains($STEMCELL_VERSION)))' > /dev/null; then
+	bosh -d mysql -n upload-stemcell "${STEMCELL_URL}${STEMCELL_VERSION}"
+fi
 
