e6a7848dc76b767988c5dc4ced51280797b34f04
s-matyukevich <s.matyukevich@gmail.com>
2018-06-27 16:02:08 -0600

    proxy upload changed

--- ci/tasks/upload-proxy-release.sh
+++ ci/tasks/upload-proxy-release.sh
@@ -2,6 +2,4 @@
 
 set -o pipefail
 
-if ! bosh --json releases | jq -e '.Tables[0].Rows[] | select(.name=="wavefront-proxy")' > /dev/null; then
-	bosh -n upload-release $WAVEFRONT_PROXY_RELEASE_URL
-fi
+bosh -n upload-release $WAVEFRONT_PROXY_RELEASE_URL
