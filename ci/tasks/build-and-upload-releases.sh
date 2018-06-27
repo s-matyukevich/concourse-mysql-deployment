16b1b90e26784d7b3d993ac608624ab853ac0f69
s-matyukevich <s.matyukevich@gmail.com>
2018-06-27 10:55:08 -0600

    telegraf agent release added

--- ci/tasks/build-and-upload-releases.sh
+++ ci/tasks/build-and-upload-releases.sh
@@ -28,12 +28,18 @@ if ! bosh --json releases | jq -e '.Tables[0].Rows[] | select(.name=="wavefront-
 	bosh -n upload-release $WAVEFRONT_PROXY_RELEASE_URL
 fi
 
-# Build custom release
-#git clone $VMWARE_MYSQL_RELEASE_URL
-#cd vmware-cf-mysql
-#bosh create-release --timestamp-version --force --tarball vmware-cf-mysql-release.tgz
-#cd ..
-#bosh -d mysql -n upload-release vmware-cf-mysql/cf-mysql-release.tgz
+git clone $TELEGRAF_AGENT_RELEASE_URL
+cd telegraf-agent-bosh-release
+git_commit=$(git rev-parse  --short  HEAD)
+bosh_commit=$(bosh --json releases | jq -r '[.Tables[0].Rows[] | select(.name=="telegraf-agent")][0].commit_hash' | sed s/+//g)
+
+if [ "$git_commit" != "$bosh_commit" ]; then
+	echo "------Building telegraf-agent release ------"
+	bosh create-release --timestamp-version --force --tarball telegraf-agent-release.tgz
+	bosh -n upload-release telegraf-agent-release.tgz
+fi
+cd ..
+
 
 if ! bosh --json stemcells | jq -e --arg STEMCELL_VERSION $STEMCELL_VERSION '.Tables[0].Rows[] | select(.name=="bosh-vsphere-esxi-ubuntu-trusty-go_agent" and (.version | contains($STEMCELL_VERSION)))' > /dev/null; then
 	bosh -d mysql -n upload-stemcell "${STEMCELL_URL}${STEMCELL_VERSION}"
